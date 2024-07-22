3afa539a5950267399f2cce55c71b8c42bbaa506
moesmq <melvin.moes@hva.nl>
2024-07-22 16:40:19 +0200

    Enemy can damage player

--- ASolsJourney/Assets/Scripts/Character/Character.cs
+++ ASolsJourney/Assets/Scripts/Character/Character.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public abstract class Character : MonoBehaviour
+public abstract class Character : MonoBehaviour, IDamagable
 {   
     public enum Team
     {
