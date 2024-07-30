2c4d7533693e9f94a1be6dbd01573a0eb961ed72
Melvin Moes <melv5@live.nl>
2024-07-30 20:20:33 +0200

    Working flamethrower effect

--- ASolsJourney/Assets/Scripts/Player Abilities/Flamethrower.cs
+++ ASolsJourney/Assets/Scripts/Player Abilities/Flamethrower.cs
@@ -6,9 +6,27 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Flamethrower : Ability
 {
-
+    Shooting shootingScript;
     public override void Activate(GameObject parent)
     {
-
+        instance.gameObject.SetActive(true);
+        if(shootingScript == null)
+            shootingScript = parent.GetComponent<AbilityManager>().shootingRef;
+        
+        if(shootingScript == null) Debug.Log("NO SHOOTING SCRIPT");
+        instance.transform.position = shootingScript.shootingPointTransform.position;
+        instance.transform.SetParent(shootingScript.shootingPointTransform);
+        
     }
+
+    public override void AbilityUpdate()
+    {
+        //instance.transform.SetPositionAndRotation(shootingScript.shootingPointTransform.position, shootingScript.shootingPointTransform.rotation);
+    }
+
+    public override void BeginCoolDown(GameObject parent)
+    {
+        instance.SetActive(false);
+    }
+    
 }
