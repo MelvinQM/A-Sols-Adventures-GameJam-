c2cfee5fc398a173e5c34137d98799018851072a
Melvin Moes <melv5@live.nl>
2024-07-31 05:07:17 +0200

    Working gameloop

--- ASolsJourney/Assets/Scripts/Player Abilities/TerraBlastProjectile.cs
+++ ASolsJourney/Assets/Scripts/Player Abilities/TerraBlastProjectile.cs
@@ -1,24 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class TerraBlastProjectile : BulletScript
+public class TerraBlastProjectile : MonoBehaviour
 {
-    public float spinCooldown = 0f;
-    public float timeBetween = 0.1f;
-    public float rotationSpeed = 10f;
-    void Update()
+    public float rotationsPerMinute = 100.0f;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private int damage;
+    [SerializeField] private int lifetime;
+    Animator ani;
+
+    public void Boom(float dirX, float dirY, float rot)
     {
-        if (spinCooldown < timeBetween)
+        GetComponent<Rigidbody2D>().velocity = new Vector2(dirX, dirY).normalized * bulletSpeed;
+        
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        IDamagable damagable = collision.gameObject.GetComponent<IDamagable>();
+        if (damagable != null && damagable.GetTeam() == Character.Team.Enemy)
         {
-            spinCooldown += Time.deltaTime;
-        }
-        else
-        {
-            spinCooldown = 0f;
-            transform.Rotate(0f, 0f, rotationSpeed);
+            damagable.TakeDamage(damage);
+            StartCoroutine(PlaySpawnAnimation(() =>
+            {
+                Destroy(gameObject);
+            }));
+            
         }
     }
+
+    private IEnumerator PlaySpawnAnimation(Action onComplete)
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        Animator ani = GetComponent<Animator>();
+        ani.Play("TerraBlast");
+        yield return new WaitForSeconds(ani.GetCurrentAnimatorStateInfo(0).length);
+        onComplete?.Invoke();
+    }
+    void Update()
+    {
+        transform.Rotate(0,0,rotationsPerMinute * Time.deltaTime);
+    }
+
 }
